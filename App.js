11298014409cb6382a69ed6da15de945cbaf01f2
jboro <joepboro@gmail.com>
2023-09-27 22:24:21 +1000

    Initial Layout, add README file

--- App.js
+++ App.js
@@ -1,20 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+
+import { Button, StyleSheet, Text, TextInput, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.appContainer}>
+    <View style={styles.inputContainer}>
+      <TextInput placeholder='What&apos;s your tidbit for today?' 
+                 style={styles.textInput}/>
+      <Button color='#C45071' title='Add Tidbit'/>
     </View>
+    <View style={styles.tidbitListContainer}>
+      <Text style={styles.testStyle}>List of Tidbits...</Text>
+    </View>
+
+    </View>
+   
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+ appContainer:{
+  flex: 1,
+  paddingTop: 50,
+  paddingHorizontal: 30,
+  backgroundColor: '#224573'
+ },
+ inputContainer: {
+  flex: 1,
+  flexDirection: 'row',
+  justifyContent: 'space-between',
+  alignItems: 'center',
+  marginBottom: 24,
+  borderBottomWidth: 3,
+  borderBottomColor: '#cccccc',
+ },
+ textInput:{
+  borderWidth: 2,
+  borderColor: '#A63F82',
+  width: '70%',
+  color: 'white',
+  marginRight: 6,
+  padding: 8
+ },
+ tidbitListContainer:{
+  flex: 5,
+  padding: 5,
+  paddingTop: 20
+ },
+ testStyle:{
+  color: 'white'
+ }
 });
---
+++ README.md
@@ -0,0 +1,4 @@
+# Tidbits
+> A React Native sample app (under development)
+
+Trying out React Native for a sample app for iOS and Android
